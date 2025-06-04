a9f104e7fd24743b6d00600a9f5f6f518f83dbd8
unknown <ismejia@uts.edu.co>
2025-06-04 12:04:26 -0500

    first commit

--- src/db/database.js
+++ src/db/database.js
@@ -3,10 +3,10 @@ import mysql from "promise-mysql"
 import config from "./../config.js"
 
 const connection = mysql.createConnection({
-    host : config.host,
-    database : config.host,
-    user : config.host,
-    password : config.host,
+    host: config.host,
+    database: config.database,
+    user: config.user,
+    password: config.password,
 });
 
 const getConnection = () => {
